b63dfa8389da8c77d43caf0d4bd6601298579d6a
epawlak8 <31115342+epawlak8@users.noreply.github.com>
2017-12-31 18:42:37 -0700

    add more media items and change colors

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,26 +1,71 @@
 import media
 import fresh_tomatoes
 
-toy_story = media.Movie('Toy Story',
-                        'A story of a boy and his toys that come to life',
-                        'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
-                        'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+toy_story = media.Movie(
+'Toy Story',
+'A story of a boy and his toys that come to life',
+'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 #print toy_story.storyline
 
-avatar = media.Movie('Avatar',
-                        'A marine on an alien planet',
-                        'https://www.movieposter.com/posters/archive/main/98/MPW-49246',
-                        'https://www.youtube.com/watch?v=cRdxXPV9GNQ')
+avatar = media.Movie(
+'Avatar',
+'A marine on an alien planet',
+'https://www.movieposter.com/posters/archive/main/98/MPW-49246',
+'https://www.youtube.com/watch?v=cRdxXPV9GNQ')
 
-jurassic_park = media.Movie('Jurassic Park',
-                        'A movie about how fucked people would be if dinosaurs were alive',
-                        'http://img.moviepostershop.com/jurassic-park-movie-poster-1992-1020141477.jpg',
-                        'https://www.youtube.com/watch?v=Bim7RtKXv90')
+saving_private_ryan = media.Movie(
+'Saving Private Ryan',
+'Matt Damon is being saved again by america, who rah',
+'https://images-na.ssl-images-amazon.com/images/I/7132RMjQwjL._RI_.jpg',
+'https://www.youtube.com/watch?v=zwhP5b4tD6g')
+
+jurassic_park = media.Movie(
+'Jurassic Park',
+'A movie about how fucked people would be if dinosaurs were alive',
+'https://images-na.ssl-images-amazon.com/images/I/41Iwrrga6rL.jpg',
+'https://www.youtube.com/watch?v=Bim7RtKXv90')
+
+dunkirk = media.Movie(
+'Dunkirik',
+'''A movie about the start of WW2, while British soliders try to escape the
+Nazis''',
+'https://goo.gl/Pi7Xuy',
+'https://www.youtube.com/watch?v=F-eMt3SrfFU')
+
+arrival = media.Movie(
+'Arrival',
+'''A movie about mysterious crafts that land on earth and how to communicate
+with their occupants''',
+'https://goo.gl/cBJ4hP',
+'https://www.youtube.com/watch?v=tFMo3UJ4B4g')
 #print avatar.storyline
 #avatar.show_trailer()
 
-movies = [toy_story, avatar, jurassic_park]
-tv_shows = [toy_story]
+rick_and_morty = media.Movie(
+'Rick and Morty',
+'Series about a scientist grandfather and his adventures with his grandson',
+'https://goo.gl/TB98b8',
+'https://youtu.be/Bmg2vXOQ3kM')
+
+archer = media.Movie(
+'Archer',
+'Animated series about a spy agency',
+'https://goo.gl/BXsYMG',
+'https://www.youtube.com/watch?v=ctLSMnaUh4Q')
+
+the_expanse = media.Movie(
+'The Expanse',
+'''A show set in the near future that follows the crew of the Canterbury,
+and a lone detective as they try to solve a mystery and prevent the solar
+system from heading toward all-out war.''',
+'https://goo.gl/2nzDAi',
+'https://www.youtube.com/watch?v=_vPLC2xKGTs')
+
+movies = [toy_story, avatar, jurassic_park, saving_private_ryan, dunkirk,
+arrival]
+
+tv_shows = [rick_and_morty, archer, the_expanse]
 
 #print media.Movie.__doc__
 #print media.Movie.__name__
--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -18,12 +18,22 @@ main_page_head = '''
     <script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
     <style type="text/css" media="screen">
         body {
+            background-color: black;
             padding-top: 80px;
         }
         #trailer .modal-dialog {
             margin-top: 200px;
             width: 640px;
             height: 480px;
+        }
+        .navbar-default {
+            background-color: LightSlateGrey;
+            background-image: none;
+            background-repeat: no-repeat;
+            color: white;
+        }
+        .navbar-default .navbar-brand {
+            color: white;
         }
         .hanging-close {
             position: absolute;
@@ -40,7 +50,7 @@ main_page_head = '''
             padding-top: 20px;
         }
         .movie-tile:hover {
-            background-color: #EEE;
+            background-color: LightSlateGrey;
             cursor: pointer;
         }
         .scale-media {
@@ -54,7 +64,7 @@ main_page_head = '''
             width: 100%;
             left: 0;
             top: 0;
-            background-color: white;
+            background-color: black;
         }
     </style>
     <script type="text/javascript" charset="utf-8">
@@ -104,7 +114,7 @@ main_page_content = '''
 
     <!-- Main Page Content -->
     <div class="container">
-      <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
             <a class="navbar-brand" href="#">Fresh Tomatoes Movie Trailers</a>
@@ -114,6 +124,7 @@ main_page_content = '''
     </div>
     <div class="container">
       {movie_tiles}
+      {tv_titles}
     </div>
   </body>
 </html>
@@ -124,15 +135,15 @@ main_page_content = '''
 movie_tile_content = '''
 <div class="col-md-6 col-lg-4 movie-tile text-center" data-trailer-youtube-id="{trailer_youtube_id}" data-toggle="modal" data-target="#trailer">
     <img src="{poster_image_url}" width="220" height="342">
-    <h2>{movie_title}</h2>
+    <h2><font color ='white'>{movie_title}</font></h2>
 </div>
 '''
 
 
-def create_movie_tiles_content(movies, tv_shows):
+def create_movie_tiles_content(movies):
     # The HTML content for this section of the page
     content = ''
-    for movie in movies or tv_shows:
+    for movie in movies:
         # Extract the youtube ID from the url
         youtube_id_match = re.search(
             r'(?<=v=)[^&#]+', movie.trailer_youtube_url)
@@ -145,8 +156,8 @@ def create_movie_tiles_content(movies, tv_shows):
         content += movie_tile_content.format(
             movie_title=movie.title,
             poster_image_url=movie.poster_image_url,
-            trailer_youtube_id=trailer_youtube_id
-        )
+            trailer_youtube_id=trailer_youtube_id)
+
     return content
 
 
@@ -156,7 +167,8 @@ def open_movies_page(movies, tv_shows):
 
     # Replace the movie tiles placeholder generated content
     rendered_content = main_page_content.format(
-        movie_tiles=create_movie_tiles_content(movies, tv_shows))
+        movie_tiles=create_movie_tiles_content(movies),
+        tv_titles=create_movie_tiles_content(tv_shows))
 
     # Output the file
     output_file.write(main_page_head + rendered_content)
